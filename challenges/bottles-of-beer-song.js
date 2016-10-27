6ae023a51fd4c759435d47b4a70ecce0e9b5977b
Dan Finelli <dan.finelli@hotmail.com>
2016-10-27 00:11:49 -0700

    Bottle of Beer done -bonuses

--- challenges/bottles-of-beer-song.js
+++ challenges/bottles-of-beer-song.js
@@ -22,3 +22,18 @@
 */
 
 // YOUR CODE HERE
+var numBottles = prompt("Please enter the number of bottles you'd like.", "1, 2, 3, 4...");
+
+numBottles = parseInt(numBottles);
+console.log(numBottles);
+
+function bottlesOfBeer(numBottles) {
+  for (var i = numBottles;i > 0; i--) {
+    alert(i+" bottles of beer on the wall,");
+    alert(i+" bottles of beer!");
+    alert("Take one down and pass it around,");
+    alert((i-1)+" bottles of beer on the wall..." );
+  }
+}
+
+bottlesOfBeer(numBottles);
