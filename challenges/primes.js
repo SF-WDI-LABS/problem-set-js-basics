53187baafabeca9f2d23ad44c5f8cd48f8af124a
umichdoe <johndoe@umich.edu>
2017-05-10 19:50:18 -0700

    experimented with some ideas

--- challenges/primes.js
+++ challenges/primes.js
@@ -10,6 +10,47 @@
 */
 
 // YOUR CODE HERE
+// copied code w3resource.com
+function isPrime(num) {
+    for(var i = 2; i < num; i++) {
+        if(num % i === 0) {
+            return false;
+        }
+    }
+    return num > 1;
+}
+
+console.log(num);
+
+
+
+/* 2. Using your `isPrime` function, create a function `primes(max)` that will return an array of all
+    prime numbers up to, but not exceeding a max number.
+*/
+
+// YOUR CODE HERE
+
+function primes(max) {
+  // get a number and check to see if it is a prime number
+  if (isPrime(max)) {
+    var highestPrime = max;
+  } else {
+    return max - 1;
+  }
+
+  while (isPrime(max)) {
+
+  }
+  // if it is not, -1 until it is equal to a prime number (put that in variable)
+  // then, do a formula that will list all prime numbers starting at 2 and up to prices(max i.e. 127)
+  // store those numbers in a variable with a comma,
+  // split those values to prepare for an array;
+  // put those values into an array
+
+}
+
+primes(128);
+
 
 //take the input
 // check to see if it is a prime number?
