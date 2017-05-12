641fc0f50d6baa653af89d1ba946525c3441cbc4
sridharpoddar <sridharpoddar@gmail.com>
2017-05-12 09:11:44 -0700

    Sridhar assignemnt complete git commit -m Sridha

--- challenges/bottles-of-beer-song.js
+++ challenges/bottles-of-beer-song.js
@@ -22,3 +22,16 @@
 */
 
 // YOUR CODE HERE
+
+let bottlesOfBeer = function(){ 
+  for (let i = n; i < 0; i--) {
+    if (i > 1){
+      console.log(i + "bottles of beer on the wall,");
+      console.log(i + "bottles of beer!");
+      console.log("Take one down and pass it around,");
+      console.log((i-1) + " bottles of beer on the wall...");
+    }
+  }
+
+}
+
