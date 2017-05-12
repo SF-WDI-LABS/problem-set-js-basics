752e24d55c6a4ef6d923e3287b2364c8072e3000
bschuetzle <bill_schuetzle@sbcglobal.net>
2017-05-11 22:48:59 -0700

    first working version of palindrome detector

--- challenges/palindrome-detector.js
+++ challenges/palindrome-detector.js
@@ -20,4 +20,20 @@
 
 */
 
-// YOUR CODE HERE
+function isPalindrome(str) {
+
+  //modify the string to remove possible punctuation and spaces between the words (for a phrase)
+  //and convert to lowercase
+  str = str.replace(/\W/gi, '').toLowerCase();
+
+  //reverse the string and store in a new variable
+  var reversedStr = str.split("").reverse().join("");
+
+  //check to see if the reversed string is the same, if so it is a Palindrome
+  if (reversedStr === str) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
