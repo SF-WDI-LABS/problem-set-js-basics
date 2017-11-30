13a7db28a5719a47d77db7904f74551d69431e59
AdamMenard <adammenard888@gmail.com>
2017-11-30 08:35:59 -0800

    Progress in homework Nov30

--- challenges/primes.js
+++ challenges/primes.js
@@ -10,3 +10,21 @@
 */
 
 // YOUR CODE HERE
+
+function isPrime(num) {
+  for (var i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  } return true;
+};
+
+function primes(max) {
+  let primesArray = [];
+
+  for (var i=2; i < max; i++) {
+    if (isPrime(i)) {
+      primesArray.push(i);
+    }
+  } return primesArray;
+};
